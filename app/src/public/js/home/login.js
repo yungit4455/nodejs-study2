656e773f0786fa9ca840fb6f92b01294054745d9
yungit4455 <yjhama1532@gmail.com>
2022-07-10 21:27:53 +0900

    defer, function과 화살표 함수의 호이스팅 차이

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,15 @@
 'use strict';
 
+const id = document.querySelector('#id');
+const password = document.querySelector('#password');
+const loginBtn = document.querySelector('button');
 
-console.log("Hell node");
+loginBtn.addEventListener('click', login);
+
+function login() {
+    const req = {
+        id: id.value,
+        password: password.value,
+    };
+    console.log(req);
+}
