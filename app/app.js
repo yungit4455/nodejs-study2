9283689e595dc819d44698e42f7affb7dcdf17b4
yungit4455 <yjhama1532@gmail.com>
2022-07-10 21:02:38 +0900

    폴더 구조 변경

--- app/app.js
+++ app/app.js
@@ -6,10 +6,10 @@ const app = express();
 
 const PORT = 3000;
 // 라우팅
-const home = require('./routes/home');
+const home = require('./src/routes/home');
 
 // 앱 세팅
-app.set('views', './views');
+app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 app.use('/', home); // use -> middleware 등록하는 method
