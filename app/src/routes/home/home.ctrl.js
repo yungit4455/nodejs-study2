2abe5b23ad52d025cba4da776d0c0373da20eec4
yungit4455 <yjhama1532@gmail.com>
2022-07-12 20:34:26 +0900

    User 모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const UserStorage = require('../../models/UserStorage');
+const User = require('../../models/User');
 
 const output = {
     home: (req, res) => {
@@ -15,23 +15,27 @@ const output = {
 // req: 프론트엔드에서 전달한 요청(request)의 데이터를 담아두는 변수
 const process = {
     login: (req, res) => {
-        const id = req.body.id;
-        const password = req.body.password;
-
-        const users = UserStorage.getUsers('id', 'password');
-
-        const response = {};
-        if (users.id.includes(id)) {
-            const idx = users.id.indexOf(id);
-            if (users.password[idx] === password) {
-                response.success = true;
-                return res.json(response);
-            }
-        }
-
-        response.success = false;
-        response.msg = '로그인에 실패하였습니다.';
+        const user = new User(req.body);
+        const response = user.login();
         return res.json(response);
+
+        // const id = req.body.id;
+        // const password = req.body.password;
+
+        // const users = UserStorage.getUsers('id', 'password');
+
+        // const response = {};
+        // if (users.id.includes(id)) {
+        //     const idx = users.id.indexOf(id);
+        //     if (users.password[idx] === password) {
+        //         response.success = true;
+        //         return res.json(response);
+        //     }
+        // }
+
+        // response.success = false;
+        // response.msg = '로그인에 실패하였습니다.';
+        // return res.json(response);
     },
 };
 
