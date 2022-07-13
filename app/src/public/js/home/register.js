56649fa2ab3108e0e1a64eb93fb475af929b5bee
yungit4455 <yjhama1532@gmail.com>
2022-07-13 17:38:26 +0900

    회원가입 요청 구현 - form 안의 button의 type(submit/button/reset/menu)은 기본 submit이다.

---
+++ app/src/public/js/home/register.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const id = document.querySelector('#id');
+const name = document.querySelector('#name');
+const password = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirm-password');
+const registerBtn = document.querySelector('button');
+
+registerBtn.addEventListener('click', register);
+
+function register() {
+    const req = {
+        id: id.value,
+        name: name.value,
+        password: password.value,
+        confirmPassword: confirmPassword.value,
+    };
+    
+    fetch('/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(req),
+    })
+    .then((res) => res.json())
+    .then((res) => {
+        if (res.success) {
+            location.href = '/';
+        } else {
+            alert(res.msg);
+        }
+    })
+    .catch((err) => {
+        console.error(new Error('로그인 도중 에러 발생!'));
+    });
+}
