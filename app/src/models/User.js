a2a60e0ab36616e424cea4c439e84bf2fce34b05
yungit4455 <yjhama1532@gmail.com>
2022-07-15 22:15:36 +0900

    AWS RDS와 mysql 연동 & dotenv로 환경변수 등록
    mysql, dotenv 의존성 추가

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -8,20 +8,24 @@ class User {
     }
 
     async login() {
-        const client = this.body;
         // await: Promise를 반환하는 곳에서만 사용할 수 있다. 비동기적인 특성을 갖기 때문에 Promise를 받는 곳에서는
         // await을 사용해야 한다. .then()으로 접근할 수도 있으나 가독성이 좋아 await을 사용했다.
         // <Pending>: Promise 반환에 시간이 오래걸리기 때문에 해당 함수는 await으로 대기를 해야한다.
-        const { id, password } = await UserStorage.getUserInfo(client.id);
-
-        // id: DB에 저장되어있는 id, body.id: Client가 입력한 id
-        if (id) {
-            if (id === client.id && password === client.password) {
-                return { success: true };
+        const client = this.body;
+        try {
+            const user = await UserStorage.getUserInfo(client.id);
+    
+            // id: DB에 저장되어있는 id, body.id: Client가 입력한 id
+            if (user) {
+                if (user.id === client.id && user.password === client.password) {
+                    return { success: true };
+                }
+                return { success: false, msg: '비밀번호가 틀렸습니다.' };
             }
-            return { success: false, msg: '비밀번호가 틀렸습니다.' };
+            return { success: false, msg: '존재하지 않는 아이디입니다.' };
+        } catch (err) {
+            return { success: false, msg: err };
         }
-        return { success: false, msg: '존재하지 않는 아이디입니다.' };
     }
 
    async register() {
--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,81 +1,27 @@
 'use strict';
 
-// Promise: 수행하는 동작이 끝남과 동시에 상태를 알려주어 비동기 처리에 효과적이다.
-const fs = require('fs').promises;
+const db = require('../config/db');
 
 class UserStorage {
-    // Code convention: 은닉화된 변수나 메서드는 클래스 최상단에 위치한다.
-    static #getUserInfo(data, id) {
-        const users = JSON.parse(data);
-        const idx = users.id.indexOf(id);
-        const usersKeys = Object.keys(users); // [id, password, name]
-        const userInfo = usersKeys.reduce((newUser, info) => {
-            newUser[info] = users[info][idx];
-            return newUser;
-        }, {});
-        
-        return userInfo;
-    }
-
-    static #getUsers(data, isAll, fields) {
-        const users = JSON.parse(data);
-        if (isAll) return users;
-
-        // reduce의 newUsers: fields 배열의 초기값, field: 다음 변수 대입
-        const newUsers = fields.reduce((newUsers, field) => {
-            if (users.hasOwnProperty(field)) {
-                newUsers[field] = users[field];
-            }
-            return newUsers;
-        }, {});
-        return newUsers;
-    }
-// 컨트롤러는 이러한 데이터를 갖고 있으면 안된다. 모델로 분리하여 모델에 데이터 저장
-// # == private
-// static: 클래스의 변수 선언 없이 클래스 자체에서 property나 method에 접근할 수 있게 하는 키워드
-    // static #users = {
-    //     id: ['yungit', 'random', 'pepe'],
-    //     password: ['1234', '1111', '1234222'],
-    //     name: ["윤준현", "랜덤", "페페"],
-    // };
-
-    // 여러 개의 arguments를 배열 fields라는 parameter로 받아온다.
-    static getUsers(isAll, ...fields) {
-        return fs.readFile("./src/databases/users.json")
-        .then((data) => {
-            return this.#getUsers(data, isAll, fields);
-        })
-        .catch(console.error);
-
-
-    }
-
     static getUserInfo(id) {
-        // const users = this.#users;
-        // .then(): Promise 로직이 성공했을 때 실행 / .catch(): 실패했을 때 실행
-        return fs.readFile("./src/databases/users.json")
-        .then((data) => {
-            return this.#getUserInfo(data, id);
-        })
-        .catch(console.error);
-        // .catch((err) => console.error(err)); <- 함수를 실행할 때 파라미터를 그대로 실행한다면 생략 가능
+        // fs는 자체적으로 Promise를 지원하지만 mysql은 미지원이라 직접 만들어야 한다.
+        return new Promise((resolve, reject) => {
+            const query = 'SELECT * FROM users WHERE id = ?;';
+            db.query(query, [id], (err, data) => {
+                if (err) reject(err);
+                resolve(data[0]);
+            });
+        });
     }
 
     static async save(userInfo) {
-        const users = await this.getUsers(true);
-        if (users.id.includes(userInfo.id)) {
-            throw '이미 존재하는 아이디입니다.';
-        }
-        users.id.push(userInfo.id);
-        users.password.push(userInfo.password);
-        users.name.push(userInfo.name);
-        fs.writeFile('./src/databases/users.json', JSON.stringify(users));
-        return { success: true };
-        // // const users = this.#users;
-        // users.id.push(userInfo.id);
-        // users.password.push(userInfo.password);
-        // users.name.push(userInfo.name);
-        // return { success: true };
+        return new Promise((resolve, reject) => {
+            const query = 'INSERT INTO users(id, name, password) VALUES(?, ?, ?);';
+            db.query(query, [userInfo.id, userInfo.name, userInfo.password], (err) => {
+                if (err) reject(`${err}`);
+                resolve({ success: true });
+            });
+        });
     }
 }
 
