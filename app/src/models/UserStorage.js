b7f42017a92376b3439a402bbd0b8f34036e31b1
yungit4455 <yjhama1532@gmail.com>
2022-07-19 21:02:28 +0900

    백엔드 맛보기 최종 최적화- render 메서드가 반환하는 HTTP 상태코드의 간략한 학습 로그 관련 최적화: 코드 가독성 향상 및 상황에 맞는 변수 삽입 테스트를 위한 의도적인 DB 에러 복구: UserStorage

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -6,9 +6,9 @@ class UserStorage {
     static getUserInfo(id) {
         // fs는 자체적으로 Promise를 지원하지만 mysql은 미지원이라 직접 만들어야 한다.
         return new Promise((resolve, reject) => {
-            const query = 'SELECT * FROM abc WHERE id = ?;';
+            const query = 'SELECT * FROM users WHERE id = ?;';
             db.query(query, [id], (err, data) => {
-                if (err) reject(err);
+                if (err) reject(`${err}`);
                 else resolve(data[0]);
             });
         });
@@ -16,7 +16,7 @@ class UserStorage {
 
     static async save(userInfo) {
         return new Promise((resolve, reject) => {
-            const query = 'INSERT INTO abc(id, name, password) VALUES(?, ?, ?);';
+            const query = 'INSERT INTO users(id, name, password) VALUES(?, ?, ?);';
             db.query(query, [userInfo.id, userInfo.name, userInfo.password], (err) => {
                 if (err) reject(`${err}`);
                 else resolve({ success: true });
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -5,17 +5,17 @@ const User = require('../../models/User');
 
 const output = {
     home: (req, res) => {
-        logger.info(`GET / 200 "홈 화면으로 이동"`);
+        logger.info(`GET / 304 "홈 화면으로 이동"`);
         res.render('home/index');
     },
     
     login: (req, res) => {
-        logger.info(`GET /login 200 "로그인 화면으로 이동"`);
+        logger.info(`GET /login 304 "로그인 화면으로 이동"`);
         res.render('home/login');
     },
 
     register: (req, res) => {
-        logger.info(`GET /register 200 "회원가입 화면으로 이동"`);
+        logger.info(`GET /register 304 "회원가입 화면으로 이동"`);
         res.render('home/register');
     }
 };
@@ -26,29 +26,46 @@ const process = {
      login: async (req, res) => {
         const user = new User(req.body);
         const response = await user.login();
-        if (response.err)
-            logger.error(`POST /login 200 Response: "success: ${response.success}, msg: ${response.err}"`);
-        else
-            logger.info(
-                `POST /login 200 Response: "success: ${response.success}, msg: ${response.msg}"`
-            );
-        return res.json(response);
+
+        const url = {
+            method: 'POST',
+            path: '/login',
+            status: response.err ? 400 : 200, // response.err가 존재하면(에러가 발생했다면) 400, 아니면 200
+        };
+
+        log(response, url);
+        return res.status(url.status).json(response);
     },
     
     register: async (req, res) => {
         const user = new User(req.body);
         const response = await user.register();
-        if (response.err)
-            logger.error(`POST /register 200 Response: "success: ${response.success}, msg: ${response.err}"`);
-        else
-            logger.info(
-                `POST /register 200 Response: "success: ${response.success}, msg: ${response.msg}"`
-            );
-        return res.json(response);
+
+        const url = {
+            method: 'POST',
+            path: '/register',
+            status: response.err ? 400 : 201,
+        };
+
+        log(response, url);
+        return res.status(url.status).json(response);
     },
 };
 
 module.exports = {
     output,
     process,
+};
+
+const log = (response, url) => {
+    if (response.err) {
+        logger.error(
+            `${url.method} ${url.path} ${url.status} Response: ${response.success}, ${response.err}`
+        );
+    } else {
+        logger.info(
+            `${url.method} ${url.path} ${url.status} Response: ${response.success}, ${
+                response.msg || ''}`
+        );
+    }
 };
